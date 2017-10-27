95a2f68d78222b73200d5627531092dceb19c856
Bin He <emptyhb@gmail.com>
2017-10-27 16:51:11 -0400

    MDS plot and single gene plots

--- Ex009-Pi-timecourse/seq-result/code/R_subfunctions_20171026.R
+++ Ex009-Pi-timecourse/seq-result/code/R_subfunctions_20171026.R
@@ -1,9 +1,14 @@
 # this file contains subfunctions to be sourced into the main analysis for Ex009
 
-myGenePlot(dat, gene, sp = sample){
+myGenePlot <- function(lc = lcpm, genes = "CAGL0B02475g", sp = sample) {
   # this function takes in the read count matrix (normalized and transformed) and a gene ID
   # and plots the values stratified by genotype and timepoint
-  sub <- dat[gene,]   # extract the subset of genes used for plotting
-  s <- colnames(dat)  # make sure the sample information of the matrix is matched to the sample sheet 
-  df <- data.frame(sp[s,.()])
+  dat <- rbind( lc[genes,sp$Sample] )   # extract the subset of genes used for plotting
+  rownames(dat) <- genes
+  dt <- data.table(sp[,.(Sample, Genotype, Timepoint)], t(dat))
+  dtm <- melt(dt, id = 1:3, variable.name = "GeneID")
+  p <- ggplot( dtm, aes( x = Timepoint, y = value, color = Genotype ) )
+  print( 
+    p + geom_point() + geom_smooth(method = "loess", aes(x = as.numeric(Timepoint), y = value, color = Genotype)) + ylab("log2 count per million") + facet_wrap( ~ GeneID ) + theme(axis.text.x = element_text(angle = 90, size = rel(0.5)))
+  )
 }
