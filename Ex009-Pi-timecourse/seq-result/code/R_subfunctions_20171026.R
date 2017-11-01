f8c3904cd94643ee652c7e790362c43976952583
Bin He <emptyhb@gmail.com>
2017-11-01 16:56:06 -0400

    concluded 2013vs2017 comparison for del80

--- Ex009-Pi-timecourse/seq-result/code/R_subfunctions_20171026.R
+++ Ex009-Pi-timecourse/seq-result/code/R_subfunctions_20171026.R
@@ -12,3 +12,38 @@ myGenePlot <- function(lc = lcpm, genes = "CAGL0B02475g", sp = sample) {
     p + geom_point() + geom_smooth(method = "loess", aes(x = as.numeric(Timepoint), y = value, color = Genotype)) + ylab("log2 count per million") + facet_wrap( ~ GeneID ) + theme(axis.text.x = element_text(angle = 90, size = rel(0.5)))
   )
 }
+
+myPlotRLE <- function(x,...) {
+  # this source code is copied from the EDASeq package (https://github.com/drisso/EDASeq/blob/master/R/methods-SeqExpressionSet.R)
+  y <- log(x+1)
+  median <- apply(y, 1, median)
+  rle <- apply(y, 2, function(x) x - median)
+  
+  boxplot(rle, ...)
+  abline(h=0, lty=2)
+  invisible(rle)
+}
+
+myBetweenLaneNormalization <- function(x, which=c("median", "upper", "full"), offset=FALSE, round=TRUE) {
+  which <- match.arg(which)
+  if(which=="full") {
+    retval <- normalizeQuantileRank(as.matrix(x), robust=TRUE)
+  } else {
+    if(which=="upper") {
+      sum <- apply(x, 2, quantile, 0.75)
+    } else {
+      sum <- apply(x, 2, median)
+    }
+    sum <- sum/mean(sum)
+    retval <- scale(x, center=FALSE, scale=sum)
+  }
+  if(!offset) {
+    if(round) {
+      retval <- round(retval)
+    }
+    return(retval)
+  } else {
+    ret <- log(retval + 0.1) - log(x + 0.1)
+    return(ret)
+  }
+}
