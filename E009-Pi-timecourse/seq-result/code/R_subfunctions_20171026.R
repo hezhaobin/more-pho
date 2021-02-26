22f7dbbf5bce7f8f220c04131b47be6e1b73711d
Bin He <emptyhb@gmail.com>
2021-02-26 15:02:31 -0600

    make shiny app for searching timecourse Pi starvation

--- E009-Pi-timecourse/seq-result/code/R_subfunctions_20171026.R
+++ E009-Pi-timecourse/seq-result/code/R_subfunctions_20171026.R
@@ -1,16 +1,16 @@
 # this file contains subfunctions to be sourced into the main analysis for Ex009
 
-myGenePlot <- function(lc = lcpm, genes = "CAGL0B02475g", sp = sample) {
+myGenePlot <- function(lc = lcpm, genes = "CAGL0B02475g", sp = sample, annotation = cgToSc) {
   # this function takes in the read count matrix (normalized and transformed) and a gene ID
   # and plots the values stratified by genotype and timepoint
-  dat <- rbind( lc[genes,sp$Sample] )   # extract the subset of genes used for plotting
-  rownames(dat) <- genes
+  dat <- lc[genes,sp$Sample]   # extract the subset of genes used for plotting
+  rownames(dat) <- paste(genes, annotation[genes, ifelse(is.na(cgname),scname,cgname)], sep = " ")
   dt <- data.table(sp[,.(Sample, Genotype, Timepoint)], t(dat))
   dtm <- melt(dt, id = 1:3, variable.name = "GeneID")
   p <- ggplot( dtm, aes( x = Timepoint, y = value, color = Genotype ) )
-  print( 
-    p + geom_point() + geom_smooth(method = "loess", aes(x = as.numeric(Timepoint), y = value, color = Genotype)) + ylab("log2 count per million") + facet_wrap( ~ GeneID ) + theme(axis.text.x = element_text(angle = 90, size = rel(0.5)))
-  )
+  p <- p + geom_point() + geom_smooth(method = "loess", formula = y ~ x, aes(x = as.numeric(Timepoint), y = value, color = Genotype)) + 
+    ylab("log2 count per million") + facet_wrap( ~ GeneID ) + theme_cowplot() + theme(axis.text.x = element_text(angle = 45, size = rel(0.75), hjust = 1))
+  print(p + panel_border())
 }
 
 myPlotRLE <- function(x,...) {
